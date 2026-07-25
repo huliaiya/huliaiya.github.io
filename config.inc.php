9ec186509781f1facef4d75f86a970939208d038
huliaiya <huangshaoyehuang@gmail.com>
2026-07-26 03:15:07 +0800

    Extract: config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+define('__ROOT_DIR__', dirname(__FILE__));
+
+define('__TEMPLATE_DIR__', __ROOT_DIR__ . '/Template');
+
+define('__ADMIN_DIR__', __ROOT_DIR__ . '/Template/Admin');
+
+define('__INCLUDE_DIR__', __ROOT_DIR__ . '/Include');
+
+define('__CORE_DIR__', __ROOT_DIR__ . '/Core');
